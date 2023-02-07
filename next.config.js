54eb4580eecc15edcf0c7b8545c8a8eab0b26f8f
Kevin Espitia <kespitia@verano.com.co>
2023-02-07 17:40:50 -0500

    :tada: init commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/home",
+        destination: "/",
+      },
+      {
+        source: "/login",
+        destination: "/login",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
